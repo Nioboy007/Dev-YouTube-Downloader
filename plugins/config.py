22dd3e4aba345203a98c071f4ca7a5ee984994eb
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-04-14 13:33:58 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -1,4 +1,15 @@
 import os
+import re
+from os import environ, getenv
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
 
 import logging
 
